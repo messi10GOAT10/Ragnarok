30ffded857fc30572cdcd4c505c3b26a86c592c5
Alex <alexsemion@gmail.com>
2026-01-10 09:48:14 -0500

    finished explore js

--- script.js
+++ script.js
@@ -59,3 +59,35 @@ cardBtn.forEach(function(element){
 modalClose.addEventListener('click', function(){
   overlay.classList.remove('overlay-active')
 })
+
+// explore
+
+const minSettings = ['Intel i5-2500k (4 core 3.3 GHz) or AMD Ryzen 3 1200 (4 core 3.1 GHz)','8 GB','Windows 10 64-bit','NVIDIA GTX 960 (4 GB) or AMD R9 290X (4 GB)','5.1','5.1','70 GB','4 GB']
+const recommendedSettings = ['Intel i5-2500k (5 core 3.3 GHz) or AMD Ryzen 4 2200 (4 core 4.1 GHz)','16 GB','Windows 11 64-bit','NVIDIA GTX 960 (5 GB) or AMD R9 290X (4 GB)','6.1','6.1','72 GB','6 GB']
+let pcCount = 1
+let psCount = 1
+
+const pcSwitcher = document.querySelector('.pcSwitcher')
+const listPC = document.querySelectorAll('.pc')
+
+pcSwitcher.addEventListener('click', function(){
+  pcCount++
+  pcSwitcher.firstElementChild.classList.toggle('switcher-active')
+  listPC.forEach((element, index) => {
+  pcCount % 2 == 0 ? element.textContent = recommendedSettings[index] : element.textContent = minimumSettings[index]
+  });
+})
+
+const standardSettings = ['PS4','20.4.2018','Sony Interactive Entertainment Europe','Action, Adventure','English, Polish, Russian','English, Dutch, Polish, Russian, Turkish']
+const limitedSettings = ['PS5','10.5.2018','Sony Interactive Entertainment Europe','Prof, Adventure','English, Polish, Russian,Ukrainian,Dutch','English,Polish, USa buy NOW']
+
+const psSwitcher = document.querySelector('.psSwitcher')
+const listPS = document.querySelectorAll('.ps')
+
+psSwitcher.addEventListener('click', function(){
+  psCount++
+  psSwitcher.firstElementChild.classList.toggle('switcher-active')
+  listPS.forEach((element, index) => {
+  psCount % 2 == 0 ? element.textContent = limitedSettings[index] : element.textContent = standardSettings[index]
+  });
+})
